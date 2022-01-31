51642fb1a3d38935db37307f137ccd03cdc0ebd0
Mati Contreras <maticontreras89.mc@gmail.com>
2022-01-30 23:46:49 -0300

    navbar

---
+++ src/components/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './navbar.css'
+import Logo from '../assets/logo-editado.svg'
+{/*import {ReactComponent as ReactLogo} from '../assets/logo-editado.svg';*/}
+
+
+const Navbar = () => {
+    return (
+      <div className="navbar">
+            <div className="contenedor-logo">
+            {/*<ReactLogo/>*/}
+            <img className="logo" src={Logo}/>
+            </div>
+            <div className="contenedor-links">
+                <li>
+                    <a className="link" src="#">Inicio</a>
+                </li>
+                <li>
+                    <a className="link" src="#">Productos</a>
+                </li>
+                <li>
+                    <a className="link" src="#">Quienes Somos</a>
+                </li>
+                <li>
+                    <a className="link" src="#">Servicios</a>
+                </li>
+                <li>
+                    <a className="link" src="#">Contacto</a>
+                </li>
+            </div>
+            <div className="contenedor-signin">
+                <button>Sign In</button>
+            </div> 
+       </div> 
+
+  
+    );
+};
+
+
+export default Navbar;
