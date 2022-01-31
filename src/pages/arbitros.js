8106cc944f45d6f92e98526557519146beadf7e0
maticontreras <maticontreras89.mc@gmail.com>
2022-01-31 00:06:40 -0300

    Add files via upload

---
+++ src/pages/arbitros.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Arbitros = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Arbitros.</h1>
+    </div>
+  );
+};
+  
+export default Arbitros;
---
+++ src/pages/canchas.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Canchas = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Canchas.</h1>
+    </div>
+  );
+};
+  
+export default Canchas;
