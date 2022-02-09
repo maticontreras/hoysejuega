8b7ab54ab43469b035f6d3cfe2ac3a153dc1ac24
maticontreras <maticontreras89.mc@gmail.com>
2022-02-09 11:21:42 -0500

    Add files via upload

---
+++ src/components/body/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ListContainer from '../ListContainer/ListContainer'
+import './body.css'
+
+const Body = () => {
+    return (
+        <div className="main-container">
+             <ListContainer/>
+        </div>
+    )
+}
+
+export default Body
